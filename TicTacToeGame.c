042433f8ce5144aa385ac36bcb2d9ab619622bb7
LamLieu <lieulam@hotmail.com>
2018-04-19 17:11:21 -0700

    Completed, unsure about a few things

--- TicTacToeGame.c
+++ TicTacToeGame.c
@@ -1,44 +1,19 @@
 #include <stdio.h>
 
 void displayBoard(char array[3][3]);
-int checkWin(char array[3][3], char playerChar);
 int checkRowWin(char array[3][3], char playerChar);
 int checkColumnWin(char array[3][3], char playerChar);
 int checkDiagonalWin(char array[3][3], char playerChar);
-
+int checkTie(char array[3][3]);
 
 void displayBoard(char array[3][3]) {
 	for (int row = 0; row < 3; row++) {
 		for (int column; column < 3; column++) {
 			printf("%c ", array[row][column]);
+			fflush(stdout);
 		}
 		printf("\n");
 	}
-}
-
-int checkWin(char array[3][3], char playerChar) {
-	if (checkRowWin(array, playerChar) == 3) {
-		return 1;
-	}
-	if (checkColumnWin(array, playerChar) == 3) {
-		return 1;
-	}
-	if (checkDiagonalWin(array, playerChar) == 3) {
-		return 1;
-	}
-	return 0;
-}
-
-int checkRowWin(char array[3][3], char playerChar) {
-	int count = 0;
-	for (int columnIndex = 0; columnIndex < 3; columnIndex++) {
-		for (int rowIndex = 0; rowIndex < 3; rowIndex++) {
-			if (array[rowIndex][columnIndex] == playerChar) {
-               count++;
-			}
-		}
-	}
-	return count;
 }
 
 int checkColumnWin(char array[3][3], char playerChar) {
@@ -53,21 +28,45 @@ int checkColumnWin(char array[3][3], char playerChar) {
 	return count;
 }
 
+int checkRowWin(char array[3][3], char playerChar) {
+	int count = 0;
+	for (int columnIndex = 0; columnIndex < 3; columnIndex++) {
+		for (int rowIndex = 0; rowIndex < 3; rowIndex++) {
+			if (array[rowIndex][columnIndex] == playerChar) {
+               count++;
+			}
+		}
+	}
+
+	return count;
+}
+
 int checkDiagonalWin(char array[3][3], char playerChar) {
 	int count = 0;
-	for (int rowIndex = 0, columnIndex = 0; rowIndex < 3 || columnIndex < 3; rowIndex++, columnIndex++) {
+	for (int rowIndex = 0, columnIndex = 0; rowIndex < 3 && columnIndex < 3; rowIndex++, columnIndex++) {
 		if (array[rowIndex][columnIndex] == playerChar) {
 			count++;
 		}
 	}
+
 	return count;
+}
+
+int checkTie(char array[3][3]) {
+	for (int row = 0; row < 3; row++) {
+		for (int column = 0; column < 3; column++) {
+			if (array[row][column] == '*') {
+				return 0;
+			}
+		}
+	}
+	return 1;
 }
 
 int main() {
 	char array[3][3];
 	int player = 1; // odd for player 1, even for player 2
-	int win = 0; // 0 for no win, 1 for win
-	int row, column;
+	int win = 0, tie = 0; // 1 = true, 0 = false
 
 	//Initializing board with asterisk
 	for (int i = 0; i < 3; i++) {
@@ -76,42 +75,73 @@ int main() {
 		}
 	}
 
-	while (win == 0) {
+	while (win == 0 && tie == 0) {
+		int row, column;
 		char playerChar = 'X';
 
-		if (player % 2 == 1) {
-			playerChar = 'O';
-			while(row > 3 || row < 1) {
-				printf("Player 2, please enter a row (1-3): ");
-				scanf("%d", &row);
-			}
-
-			while(column > 3 || column < 1) {
-				printf("Player 2, please enter a column (1-3): ");
-				scanf("%d", &column);
-			}
-			array[row - 1][column - 1] = 'O';
-		}
-		else {
+		if (player % 2 == 1) { //Player 1
 			playerChar = 'X';
-			while(row > 3 || row < 1) {
-				printf("Player 1, please enter a row (1-3): ");
-				scanf("%d", &row);
-			}
 
-			while(column > 3 || column < 1) {
-				printf("Player 1, please enter a column (1-3): ");
-				scanf("%d", &column);
-			}
-			array[row - 1][column - 1] = 'X';
+			do {
+				while(row > 3 || row < 1) {
+					printf("Player 1, please enter a row (1-3): ");
+					fflush(stdout);
+					scanf("%d", &row);
+				}
+				while(column > 3 || column < 1) {
+					printf("Player 1, please enter a column (1-3): ");
+					fflush(stdout);
+					scanf("%d", &column);
+				}
+
+				if (array[row - 1][column - 1] == '*') {
+					array[row - 1][column - 1] = 'X';
+				}
+			} while (array[row - 1][column - 1] != '*');
 		}
+		else { //Player 2
+			playerChar = 'O';
+
+			do {
+				while(row > 3 || row < 1) {
+					printf("Player 2, please enter a row (1-3): ");
+					fflush(stdout);
+					scanf("%d", &row);
+				}
+				while(column > 3 || column < 1) {
+					printf("Player 2, please enter a column (1-3): ");
+					fflush(stdout);
+					scanf("%d", &column);
+				}
+
+				if (array[row - 1][column - 1] == '*') {
+					array[row - 1][column - 1] = 'O';
+				}
+			} while (array[row - 1][column - 1] != '*');
+		}
+
+		printf("fdsafds");
 		displayBoard(array);
-		win = checkWin(array, playerChar);
+
+		if (checkRowWin(array, playerChar) == 3) {
+			win = 1;
+		}
+		if (checkColumnWin(array, playerChar) == 3) {
+			win = 1;
+		}
+		if (checkDiagonalWin(array, playerChar) == 3) {
+			win = 1;
+		}
+
+		tie = checkTie(array);
 
 		if (win == 1) {
-			printf("Player %d has won!", (player % 2) + 1);
+			printf("Player %d has won the game!\n", (player % 2) + 1);
 		}
 
+		if (tie == 1) {
+			printf("It's a tie!\n");
+		}
 		player++;
 	}
 
